967279bdbfe4af21ba29b1d9950538503536877c
karzenwospuxgalden <karzenwospuxgalden@kpwn.it>
2022-11-29 14:52:50 +0000

    Recreate start page as page component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import "yet-another-react-lightbox/styles.css";
 import type { AppProps } from 'next/app'
 import Layout from "../components/Layout";
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,38 @@
+import {useState} from "react";
+import PhotoAlbum from "react-photo-album";
+import Lightbox from "yet-another-react-lightbox";
+
 export default function Home() {
-  return (
-      <div className="ce_text block">
-        <div className="image_container float_right" style={{padding: "25px 5px 5px 35px", float: "right"}}>
-          <a href="/files/malermeister-wild/zertifikat.png" rel="lightbox">
-            <img alt="" height="200" src="/assets/images/3/zertifikat-aecb8383.png" width="143"/>
-          </a>
+    const [showGallery, setShowGallery] = useState(false);
+    const images = [{src: '/files/malermeister-wild/zertifikat.png', width: 320, height: 447}];
+    return (
+        <div className="ce_text block">
+            <div className="image_container float_right" style={{padding: "25px 10px 10px 35px", width: "200px", float: "right"}}>
+                <PhotoAlbum
+                    layout="columns"
+                    photos={images}
+                    onClick={() => setShowGallery(true)}
+                />
+                <Lightbox
+                    open={showGallery}
+                    close={() => setShowGallery(false)}
+                    slides={images}
+                    render={{
+                        buttonPrev: () => null,
+                        buttonNext: () => null
+                    }}
+                />
+            </div>
+            <h2>Herzlich willkommen und vielen Dank für Ihren Besuch auf meiner Homepage.</h2>
+            <p>Liebevoll gestaltete Räume laden zum Wohlfühlen ein. Kahle weiße Wände gehören zur
+                Vergangenheit. Geben Sie Ihrem Zuhause eine individuelle Note. Egal welcher Wohnbereich,
+                durch die verschiedensten Gestaltungsmöglichkeiten, Farbtöne und Materialien bietet sich
+                eine große Auswahl von Möglichkeiten jedem Bereich seine spezielle Note zu
+                verleihen.</p>
+            <p>Bei der Umsetzung Ihrer Ideen möchte ich gern Ihr zuverlässiger Partner sein und meine
+                Erfahrungen mit einbringen. Nach einer individuellen Beratung vor Ort finden wir sicher
+                die richtige Gestaltung für Ihre Räume.</p>
+            <p>Mit freundlichen Grüßen Ihr Malermeister André Wild</p>
         </div>
-        <h2>Herzlich willkommen und vielen Dank für Ihren Besuch auf meiner Homepage.</h2>
-        <p>Liebevoll gestaltete Räume laden zum Wohlfühlen ein. Kahle weiße Wände gehören zur
-          Vergangenheit. Geben Sie Ihrem Zuhause eine individuelle Note. Egal welcher Wohnbereich,
-          durch die verschiedensten Gestaltungsmöglichkeiten, Farbtöne und Materialien bietet sich
-          eine große Auswahl von Möglichkeiten jedem Bereich seine spezielle Note zu
-          verleihen.</p>
-        <p>Bei der Umsetzung Ihrer Ideen möchte ich gern Ihr zuverlässiger Partner sein und meine
-          Erfahrungen mit einbringen. Nach einer individuellen Beratung vor Ort finden wir sicher
-          die richtige Gestaltung für Ihre Räume.</p>
-        <p>Mit freundlichen Grüßen Ihr Malermeister André Wild</p>
-      </div>
-  );
+    );
 }
