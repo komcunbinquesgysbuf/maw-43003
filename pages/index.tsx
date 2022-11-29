d8d3a26edb04d811f4a23af75cdf4947b228d4e1
karzenwospuxgalden <karzenwospuxgalden@kpwn.it>
2022-11-29 13:21:47 +0000

    Rewrite index page to use layout (lightbox missing)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,21 @@
-import {useRouter} from "next/router";
-import {useEffect} from "react";
-
 export default function Home() {
-  const {replace} = useRouter();
-  useEffect(() => {
-    replace('/startseite/index.html');
-  }, [replace]);
-  return <></>;
+  return (
+      <div className="ce_text block">
+        <div className="image_container float_right" style={{padding: "25px 5px 5px 35px", float: "right"}}>
+          <a href="/files/malermeister-wild/zertifikat.png" rel="lightbox">
+            <img alt="" height="200" src="/assets/images/3/zertifikat-aecb8383.png" width="143"/>
+          </a>
+        </div>
+        <h2>Herzlich willkommen und vielen Dank für Ihren Besuch auf meiner Homepage.</h2>
+        <p>Liebevoll gestaltete Räume laden zum Wohlfühlen ein. Kahle weiße Wände gehören zur
+          Vergangenheit. Geben Sie Ihrem Zuhause eine individuelle Note. Egal welcher Wohnbereich,
+          durch die verschiedensten Gestaltungsmöglichkeiten, Farbtöne und Materialien bietet sich
+          eine große Auswahl von Möglichkeiten jedem Bereich seine spezielle Note zu
+          verleihen.</p>
+        <p>Bei der Umsetzung Ihrer Ideen möchte ich gern Ihr zuverlässiger Partner sein und meine
+          Erfahrungen mit einbringen. Nach einer individuellen Beratung vor Ort finden wir sicher
+          die richtige Gestaltung für Ihre Räume.</p>
+        <p>Mit freundlichen Grüßen Ihr Malermeister André Wild</p>
+      </div>
+  );
 }
