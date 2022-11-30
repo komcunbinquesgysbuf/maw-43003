42bc3195a15055053daa1b2333ff6146b1218967
karzenwospuxgalden <karzenwospuxgalden@kpwn.it>
2022-11-30 00:33:02 +0000

    Extract testimonials data

--- pages/kundenmeinungen/index.tsx
+++ pages/kundenmeinungen/index.tsx
@@ -1,72 +1,12 @@
 import React, {useState} from 'react';
 import HCaptcha from "@hcaptcha/react-hcaptcha";
 import Link from "next/link";
+import testimonials from "../../data/testimonials";
 
 export default function Kundenmeinungen() {
     const [enableSubmit, setEnableSubmit] = useState(false);
     const [pageNo, setPageNo] = useState(1);
     const disableSubmit = () => setEnableSubmit(false);
-    const testimonials = [
-        {
-            id: 'c16',
-            date: '14.06.2020',
-            author: 'S.Thrun',
-            comment: <p>Sehr empfehlenswert!<br/> Top Preis/Leistungsverhältnis. <br/> Zuverlässig, sauber und genau
-                nach unseren Vorstellungen gearbeitet.</p>
-        },
-        {
-            id: 'c15',
-            date: '31.05.2020',
-            author: 'Kotulla Günther',
-            comment: <p>Top Firma.<br/> Zuverlässig, sehr sauber und schnell gearbeitet.<br/> Zu unserer vollsten
-                Zufriedenheit.<br/>Sehr empfehlenswert!</p>
-        },
-        {
-            id: 'c14',
-            date: '18.05.2020',
-            author: 'B. Bräuer',
-            comment: <p>Herr Wild hat sehr sauber, exakt und schnell gearbeitet, er war immer super pünktlich und ich
-                bin sehr begeistert von dem Ergebnis seiner Arbeit. Absolut zuverlässig und empfehlenswert!</p>
-        },
-        {
-            id: 'c12',
-            date: '04.05.2017',
-            author: 'Dr. Frank Tellkamp 4.5.2017',
-            comment: <p>Herr Wild arbeitet zuverlässig, exakt und mit hoher Qualität. Er ist stets freundlich,
-                hilfsbereit und berät fachlich kompetent. Empfehlung!</p>
-        },
-        {
-            id: 'c11',
-            date: '16.01.2016',
-            author: 'A. und R. Wienke',
-            comment: <p>Einen großen Dank an Herrn Wild. Bereits mehrfach konnten wir auf seine klasse Arbeit
-                zurückgreifen. Neben der stets ausserordentlich hohen Qualität seiner Leistungen berät er auf Wunsch
-                auch und gibt Tipps zur Umsetzung. Wir freuen uns schon heute auf das nächste Projekt ihm.</p>
-        },
-        {
-            id: 'c10',
-            date: '11.07.2014',
-            author: 'E. Bohot Physiotherapie',
-            comment: <p>Ich kann mich nur lobend über die Arbeit von Herrn Wild äußern.<br/> Exakt, Sauber,
-                Schnell,Freundlich !!<br/>Von meinen Patienten kommen ebenfalls nur positive Reaktionen.</p>
-        },
-        {
-            id: 'c9',
-            date: '19.05.2014',
-            author: 'Ellen und Dr. Günter Mildner',
-            comment: <p>Die Ausführungen von Herrn Dr. Mai können wir nur unterstreichen. Herr Wild hat auch unseren 2.
-                Auftrag zur vollen Zufriedenheit erledigt, die dabei aufgetretenen Besonderheiten fachmännisch
-                gemeistert und ist damit unseren Ansprüchen gerecht geworden.</p>
-        },
-        {
-            id: 'c8',
-            date: '23.10.2013',
-            author: 'Dr. Christoph Mai',
-            comment: <p>Herrn Wild kann man wirklich uneingeschränkt weiter empfehlen! Er arbeitet qualitativ
-                hervorragend, hält sich korrekt an alle getroffenen Absprachen, arbeitet schnell und ist dabei immer
-                sehr freundlich. Ein Malermeister, der sein Geld wirklich wert ist!</p>
-        },
-    ];
     const pageSize = 4;
     const numPages = Math.ceil(testimonials.length / pageSize);
     return (
